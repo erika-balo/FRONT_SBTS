b2c8592611835f3b8eb006cb0a7d532fdf7a8c87
Jose Manuel <josemanuelgarciadelao@gmail.com>
2025-04-10 15:04:29 -0600

    feat: cambio de entornos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URL: 'https://api.semanadelemprendedordgo.com',
-    MERCURE_URL: 'https://mercure.semanadelemprendedordgo.com/.well-known/mercure',
+    API_URL: 'https://api.digitalganadera.com',
+    MERCURE_URL: 'https://mercure.digitalganadera.com/.well-known/mercure',
     URL_IMAGENES: 'http://54.202.160.216',
 };
