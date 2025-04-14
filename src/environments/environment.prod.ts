fe7b50fa2941042e0a73939044b649dd10d9285d
Jose Manuel <josemanuelgarciadelao@gmail.com>
2025-04-14 16:20:27 -0600

    add prod entornos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
     API_URL: 'https://api.digitalganadera.com',
     MERCURE_URL: 'https://mercure.digitalganadera.com/.well-known/mercure',
     URL_IMAGENES: 'http://54.202.160.216',
+    STREAM_SERVER: 'streaming.digitalganadera.com/'
 };
